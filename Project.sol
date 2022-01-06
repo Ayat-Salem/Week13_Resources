44c0d471b96139ce7fe691f4b22782f5a7323f75
Ayat-Salem <91917437+Ayat-Salem@users.noreply.github.com>
2022-01-06 16:30:07 +0300

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -1,4 +1,16 @@
-pragma solidity ^ 0.8.0;
+pragma solidity ^ 0.4.25;
+
+contract CrowdFundingCampaignFactory{
+    address[] public deplyedCampaigns;
+
+    function createCampaign(uint minimum) public{
+        address newCampaign = new CrowdFunding(minimum, msg.sender);
+        deplyedCampaigns.push(newCampaign);
+    }
+    function getDeplyedCampaigns() public view returns (address[]){
+        return deplyedCampaigns;
+    }
+}
 
 contract CrowdFunding {
 // the following struct is for a withdrawal request.
@@ -8,8 +20,10 @@ contract CrowdFunding {
             address recipient; // in case the owner of the contract want to send the withdrawal to someone other than himself.
             bool complete; // to mark wether the withdrawl is completed or not.
             uint approvalCount; // to make sure we have the exact number of approvals (in this contract it will be more than 50% of the contributors).
+
     }
-    mapping(address => bool) approvals; // to make sure if someone has already approved he will not be able to approve again.
+         mapping(address => bool) approvals; // to make sure if someone has already approved he will not be able to approve again.
+
     withdrawal[] public withdrawals; 
     address public owner; // adreess for whoever starts the contract
     mapping (address => bool) contributors; // to keep track of all the contributors. we will have their adressess then chech if they contributed or not.
@@ -52,5 +66,26 @@ contract CrowdFunding {
         });
         withdrawals.push(newWithdrawal); // setting up an array to add the new withdrawals.
     }
+
+// the following function is created to grab the actual withdrawal request.  
+    function approveWithdrawal(uint index) public onlyContributer {
+        withdrawal memory thisWithdrawal = withdrawals[index]; // each withdrawal request will have an index that helps to locate the request.
+        require(!approvals[msg.sender]); // to make sure that whoever sending the approval hasn't already approved it.
+        approvals[msg.sender]==true;
+        thisWithdrawal.approvalCount++; // increasing the approval count wehn "true".
+        
+    }
+
+    // the following function is created to transfer and draw the fund.    
+     function finalizeWithdrawal(uint index) public onlyOwner { 
+         withdrawal memory thisWithdrawal = withdrawals[index];
+         require(thisWithdrawal.approvalCount >= (contributorsCount / 2 )); // we want to require that the approval count is more than or equal to 50% of the approvals.
+         require(!thisWithdrawal.complete); // to make sure the withdrawal hasn't completed before.
+
+         thisWithdrawal.recipient.transfer(thisWithdrawal.value); // sending the withdrawal to the recipient.
+         thisWithdrawal.complete = true;
+
+     }
+
 
 }
